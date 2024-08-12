c6729f832cb65c2ff072900f5eeb567e6a25d1af
Ldv236 <ldv236@yandex.ru>
2024-08-12 22:21:43 +0200

    leetcode

---
+++ src/main/java/ru/LeetCode/Java/InsertInterval57.java
@@ -0,0 +1,41 @@
+package ru.LeetCode.Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InsertInterval57 {
+    public static void main(String[] args) {
+        int[][] res = insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5});
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(Arrays.toString(res[i]));
+        }
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        // Добавляем все интервалы, заканчивающиеся до начала нового интервала
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        // Слияние всех перекрывающихся интервалов в новый интервал
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        // Добавляем объединенный новый интервал
+        result.add(newInterval);
+
+        // Добавляем остальные интервалы
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+        // Преобразовываем список обратно в массив для возврата
+        return result.toArray(new int[result.size()][2]);
+    }
+}
---
+++ src/main/java/ru/LeetCode/find/SimpleFind.java
@@ -0,0 +1,68 @@
+package ru.LeetCode.find;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SimpleFind {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] nums1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target1 = 8;
+        String str1 = "";
+        System.out.println(linearContains(nums1, target1));
+
+
+        int[] genArr1 = new int[10_000_000];
+        for (int i = 0; i < genArr1.length; i++) {
+            genArr1[i] = i;
+        }
+        int[] genArr2 = Arrays.copyOf(genArr1, 10_000_000);
+
+        long start1 = System.currentTimeMillis();
+        System.out.println(linearFindIndex(genArr1, 9_999_888));
+        System.out.println(System.currentTimeMillis() - start1);
+
+        long start2 = System.currentTimeMillis();
+        System.out.println(binaryFindIndex(genArr2, 9_999_888));
+        System.out.println(System.currentTimeMillis() - start2);
+    }
+
+    public static boolean linearContains(int[] arr, int element) {
+        for (int i : arr) {
+            if (i == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int linearFindIndex(int[] arr, int element) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binaryFindIndex(int[] arr, int element) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (element == arr[mid]) {
+                return mid; //true
+            }
+
+            if (element < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return -1; //false
+    }
+}
