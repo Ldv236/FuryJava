205d8ff7c6aea575c0c530de4130d6b0d08ed2ac
Ldv236 <ldv236@yandex.ru>
2024-08-24 14:51:52 +0200

    any

--- src/main/java/ru/ldv236/streamApi/Main.java
+++ src/main/java/ru/ldv236/streamApi/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         StreamWithPersons streamWithPersons = new StreamWithPersons();
-//        streamWithPersons.doStream();
+        streamWithPersons.doStream();
 
         StreamWithArray streamWithArray = new StreamWithArray();
 //        streamWithArray.doStream();
 
         StreamTasks streamTasks = new StreamTasks();
-        streamTasks.doTask1();
-        streamTasks.doTask2();
-        streamTasks.doTask3();
-        streamTasks.doTask4();
-        streamTasks.doTask5();
-        streamTasks.doTask6();
+//        streamTasks.doTask1();
+//        streamTasks.doTask2();
+//        streamTasks.doTask3();
+//        streamTasks.doTask4();
+//        streamTasks.doTask5();
+//        streamTasks.doTask6();
 
     }
 }
